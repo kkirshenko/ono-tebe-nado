13b828f6b4cc70679206d8e05e8a24527c33c427
Зволибовская Катя <kkirshenko@mail.ru>
2025-04-06 01:46:34 +0300

    ono-tebe-nado

---
+++ README.md
@@ -0,0 +1,5 @@
+# Проектная работа "Оно тебе надо"
+
+Стек: HTML, SCSS, TS
+
+## [Сайт-демонстрация](https://docesforg.github.io/ono-tebe-nado/)
---
+++ src/utils/constants.ts
@@ -0,0 +1,4 @@
+export const API_URL = `https://onotebenado-api.nomoreparties.co/api/onotebenado`;
+export const CDN_URL = `https://onotebenado-api.nomoreparties.co/content/onotebenado`;
+
+export const settings = {};
